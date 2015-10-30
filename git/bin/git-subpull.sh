481d0d0a431e610925d029b688e878803b94f755
Dan Church <dchurch@g2planet.com>
2015-10-30 10:45:26 -0500

    git: Fix subpull script for hardiness
    If the branch isn't ffable, it'll fail and stop, instead of ignoring.

--- git/bin/git-subpull.sh
+++ git/bin/git-subpull.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git submodule foreach -q --recursive 'echo $name:;git checkout "$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git pull -r; echo'
+git submodule foreach -q --recursive 'echo $name:; git fetch && git checkout "$(git config -f $toplevel/.gitmodules submodule.$name.branch)" && git merge --ff-only && echo'
