455dad91c7255813205e767c86c408f89ebfbcd2
Dan Church <amphetamachine@gmail.com>
2016-02-28 12:20:13 -0600

    colors: Add helper script for neonwolf color schemes

---
+++ colors/neonwolf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# vi: et sts=4 sw=4 ts=4
+
+NEONWOLF_PALETTE=(
+	39
+	81
+	82
+	121
+	154
+	165
+	166
+	196
+	222
+	# grays
+	232
+	235
+	238 # used in tig as digraph line color, nowhere else
+	240
+	255
+)
+
+# colors that were part of the original palette, but that I've been ignoring
+BADWOLF_PALETTE_AUX=(
+	16
+	27
+	137
+	173
+	211
+	214
+	221
+	# grays
+	241
+)
+
+256colors2.pl \
+    "${NEONWOLF_PALETTE[@]}" \
+    "${BADWOLF_PALETTE_AUX[@]}"
