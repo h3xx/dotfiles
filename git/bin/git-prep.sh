6de6c4e89e80fcb8db268efda1d7bad787d7f94e
Dan Church <dchurch@g2planet.com>
2017-02-13 13:53:42 -0600

    git: Add `prep` alias

---
+++ git/bin/git-prep.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+git_basedir=$(git rev-parse --show-toplevel)
+if [[ -z $git_basedir ]]; then
+    echo 'Failed to find git root directory' 1>&2
+    exit 2
+fi
+
+# cheat the "alternates" file
+if [[ -f "$git_basedir/.gitmodules" ]]; then
+    submodules=(
+        $(git config -f "$git_basedir/.gitmodules" --get-regexp --name-only '^submodule\..*\.url$' | cut -f 2 -d .)
+    )
+    for sm in "${submodules[@]}"; do
+        localcopy=~/.gitrepos/$(basename -- "$sm").git
+        if [[ -d $localcopy/objects ]]; then
+            git submodule update --init --reference "$localcopy" "$sm" || exit
+        fi
+    done
+fi
+
+git submodule update --recursive --init || exit
+git-alts.sh
+git gc
+
+git remote rename origin o
+git submodule foreach --recursive 'git remote rename origin o;true'
