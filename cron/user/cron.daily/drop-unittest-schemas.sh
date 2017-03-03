d2622413896b92b253b2482cd30d102be946c090
Dan Church <dchurch@g2planet.com>
2017-03-03 13:50:57 -0600

    cron: Fix unittest database clearing
    Evidently dropping schemas leaves junk behind. The database directory
    grew to 14GB!

--- cron/user/cron.daily/drop-unittest-schemas.sh
+++ cron/user/cron.daily/drop-unittest-schemas.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-for schema in $(
-	psql -U unittest -c "copy (select schema_name from information_schema.schemata where schema_name like 'unittest%') to stdout" 2>/dev/null
-); do
-	psql -U unittest -c "drop schema $schema cascade" >/dev/null
-done
+psql -U postgres <<'EOSQL'
+drop database unittest;
+create database unittest;
+grant all on database unittest to unittest;
+
+EOSQL
 
 # also clear out old temporary files
 rm -f \
