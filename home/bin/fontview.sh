7148d9f3ecf65c2ab81b60f812332ac9dcf18f46
Dan Church <amphetamachine@gmail.com>
2017-05-26 08:51:42 -0500

    bin: Add fontview.sh
    It's mature enough to replicate to my work computer, it's mature enough
    for my dots.

---
+++ home/bin/fontview.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# vi: et sts=4 sw=4 ts=4
+
+HELP_MESSAGE() {
+    local EXIT_CODE=${1:-0}
+    cat <<EOF
+Usage: ${0##*/} [OPTIONS] [--] FILE...
+Display an image of a font file.
+
+  -h        Show this help message.
+  -t TEXT   Show TEXT instead of default.
+  --        Terminate options list.
+
+Copyright (C) 2015-2017 Dan Church.
+License GPLv3+: GNU GPL version 3 or later (http://gnu.org/licenses/gpl.html).
+This is free software: you are free to change and redistribute it. There is NO
+WARRANTY, to the extent permitted by law.
+EOF
+    exit "$EXIT_CODE"
+}
+
+TEXT=
+
+while getopts 't:h' flag; do
+    case "$flag" in
+        'h')
+            HELP_MESSAGE 0
+            ;;
+        't')
+            TEXT=$OPTARG
+            ;;
+        *)
+            HELP_MESSAGE 1
+            ;;
+    esac
+done
+
+shift "$((OPTIND-1))"
+
+TEMP_FILES=()
+
+cleanup() {
+    rm -rf -- "${TEMP_FILES[@]}"
+}
+
+trap 'cleanup' EXIT
+
+FONTIMAGE_ARGS=()
+
+if [[ -n $TEXT ]]; then
+    FONTIMAGE_ARGS+=(--text "$TEXT")
+fi
+
+TEMP_DIR=$(mktemp -d -t "${0##*/}.XXXXXX")
+# XXX : fontimage requires .png extension
+TEMP=$TEMP_DIR/fontimage.png
+TEMP_FILES+=("$TEMP_DIR")
+
+for FN; do
+
+    echo "$FN"
+    fontimage "${FONTIMAGE_ARGS[@]}" -o "$TEMP" "$FN" &&
+    xv "$TEMP"
+done
