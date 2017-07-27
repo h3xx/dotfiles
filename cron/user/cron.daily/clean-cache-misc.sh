ad4fbf447e66a7e04759eec3a0cc312eea4609af
Dan Church <amphetamachine@gmail.com>
2017-07-27 11:08:01 -0500

    cron: Add cache cleaner script

---
+++ cron/user/cron.daily/clean-cache-misc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# vi: et sts=4 sw=4 ts=4
+
+set -e
+
+. ~/.cron/cron.conf
+
+CACHEDIRS=(
+# auto-generated thumbnails from geeqie, probably others
+~/.thumbnails
+# Steam crash dumps - whenever a game crashes Steam adds a file here
+/tmp/dumps
+# temporary files ccache might leave behind (ccache manages the cache directory size)
+~/.ccache/tmp
+~/.cache/youtube-dl
+~/.cache/fontconfig
+~/.cache/winetricks
+)
+
+DAYS=7
+
+for DIR in "${CACHEDIRS[@]}"; do
+    if [[ -d $DIR ]]; then
+
+        (
+        # Remove old files
+        find "$DIR/" \
+            -mindepth 1 \
+            -type f \
+            ! -name .gitignore \
+            -mtime "+$DAYS" \
+            -print \
+            -delete
+
+        # Clear out any empty directories, including the cache dir itself
+        find "$DIR/" \
+            -type d \
+            -empty \
+            -print \
+            -delete
+
+        ) >>"$LOG"
+
+    fi
+done
