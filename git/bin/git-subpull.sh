65c61b0a17fbaa0226395281095d68df6ac4120c
Dan Church <dchurch@g2planet.com>
2017-08-17 12:24:32 -0500

    git: Fix subpull
    If a submodule was in a detatched HEAD state, it would fail to work.
    This fixes it by fetching from all remotes. Because I don't care.

--- git/bin/git-subpull.sh
+++ git/bin/git-subpull.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-git submodule foreach -q --recursive 'echo $name:; git fetch && git checkout "$(git config -f $toplevel/.gitmodules submodule.$name.branch)" && git merge --ff-only && echo'
+git submodule foreach -q --recursive 'echo $name:; git fetch --all && git checkout "$(git config -f $toplevel/.gitmodules submodule.$name.branch)" && git merge --ff-only && echo'
