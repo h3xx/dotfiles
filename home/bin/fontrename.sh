3004cdd1bd910ee3e99057485a377dfd1b1b20f6
Dan Church <dchurch@g2planet.com>
2019-07-05 17:44:16 -0500

    bin: Update fontrename
    Support the new mimetype for TTF files.

--- home/bin/fontrename.sh
+++ home/bin/fontrename.sh
@@ -58,7 +58,7 @@ fontname() {
 	mimetype="$(get_mimetype "$font")"
 	case "$mimetype" in
 
-		'application/font-sfnt'|'application/x-font-ttf')
+		'application/font-sfnt'|'application/x-font-ttf'|'font/sfnt')
 			# XXX : for some reason, ttf2afm requires a .ttf file
 			#	extension, so the creation of a temporary
 			#	symlink is necessary
@@ -127,7 +127,7 @@ fontext() {
 	mimetype="$(get_mimetype "$font")"
 	case "$mimetype" in
 
-		'application/font-sfnt'|'application/x-font-ttf')
+		'application/font-sfnt'|'application/x-font-ttf'|'font/sfnt')
 			echo '.ttf'
 			;;
 
