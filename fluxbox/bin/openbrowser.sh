0922b0185b6163af963613eb6cb65026c5311222
Dan Church <amphetamachine@gmail.com>
2020-05-20 10:29:10 -0500

    all: Remove squid
    Since the world moved to HTTPS, an HTTP caching proxy is less and less
    relevant.
    
    Latest usage statistics over the past month only reveals a 10.96% hit
    rate, with only 2% of the data throughput coming from the cache.
    
    It cannot even cache my resume site, which is served over HTTPS, and
    consists of static assets. The browser cache is far more effective.
    
    Sorry to see you go, buddy.

--- fluxbox/bin/openbrowser.sh
+++ fluxbox/bin/openbrowser.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 # vi: et sts=4 sw=4 ts=4
 
-SQUID=1
-
 CHROME_OPTIONS=(
     --ignore-gpu-blocklist
     --purge-memory-button
     --process-per-site
     --wm-window-animations-disabled
 )
-
-if [[ $SQUID -ne 0 ]]; then
-    CHROME_OPTIONS+=(
-        --proxy-server='http://localhost:3128'
-        --proxy-bypass-list='localhost'
-    )
-fi
 
 exec google-chrome \
     "${CHROME_OPTIONS[@]}" \
